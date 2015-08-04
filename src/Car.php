7016138a17ee4f96951f417ed51585cd2860c3c6
Tyler Austin Deem & Deron Johnson <student@epicodus.com>
2015-08-04 14:15:59 -0700

    Added car class in car.php and linked in app.php.

---
+++ src/Car.php
@@ -0,0 +1,60 @@
+<?php
+    class Car
+    {
+        private $make_model;
+        private $price;
+        private $miles;
+        private $image;
+
+        function __construct($make, $price, $miles, $image)
+        {
+            $this->make_model = $make;
+            $this->price = $price;
+            $this->miles = $miles;
+            $this->image = $image;
+        }
+
+        function setMake($new_make)
+        {
+            $this->make_model = $new_make;
+        }
+
+        function getMake()
+        {
+            return $this->make_model;
+        }
+
+        function setPrice($new_price)
+        {
+            $float_price = (float) $new_price;
+            if ($float_price != 0) {
+                $this->price = $float_price;
+            }
+        }
+
+        function getPrice()
+        {
+            return $this->price;
+        }
+
+        function setMiles($new_miles)
+        {
+            $this->miles = $new_miles;
+        }
+
+        function getMiles()
+        {
+            return $this->miles;
+        }
+
+        function setImage($new_image)
+        {
+            $this->image = $new_image;
+        }
+
+        function getImage()
+        {
+            return $this->image;
+        }
+    }
+?>

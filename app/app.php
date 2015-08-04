1645cc6b1220e898c30947d11aa08b5a32abedc7
Tyler Austin Deem & Deron Johnson <student@epicodus.com>
2015-08-04 15:53:02 -0700

    added car results app with instances of cars + images

--- app/app.php
+++ app/app.php
@@ -15,7 +15,7 @@
         <body>
           <h1>Find YOUR new car...</h1>
           <div class='container'>
-            <form action='car.php'>
+            <form action='/car'>
               <div class='form-group'>
                 <label for='price'>Enter your max price: $</label>
                 <input id='price' name='price' class='form-control' type='number'>
@@ -30,6 +30,57 @@
         </body>
         </html>
             ";
+    });
+
+    $app->get("/car", function() {
+        $first_car = new Car("Toyota Camry", 5500, 160000, "img/camry.jpg");
+        $second_car = new Car("Honda CRV", 7500, 140000, "img/crv.jpg");
+        $third_car = new Car("Suburu Legacy", 7900, 130000, "img/legacy.jpg");
+        $fourth_car = new Car("Tesla Model S", 200000, 5000, "img/tesla.jpg");
+
+        $cars = array($first_car, $second_car, $third_car, $fourth_car);
+
+        $cars_matching_search = array();
+        foreach ($cars as $car) {
+          $car_price = $car->getPrice();
+          $car_miles = $car->getMiles();
+          if (empty($_GET["price"])) {
+            if ($car_miles < $_GET["miles"]) {
+              array_push($cars_matching_search, $car);
+            }
+          } elseif (empty($_GET["miles"])) {
+              if ($car_price < $_GET["price"]) {
+                array_push($cars_matching_search, $car);
+              }
+          } elseif ($car_price < $_GET["price"] && $car_miles < $_GET["miles"]) {
+            array_push($cars_matching_search, $car);
+              }
+          }
+
+          $output = "";
+
+
+          if (empty($cars_matching_search)) {
+                  echo "<h1>Uh oh! No cars available for you.<h1>";
+              } else {
+                foreach ($cars_matching_search as $car) {
+                    $car_make = $car->getMake();
+                    $car_price = $car->getPrice();
+                    $car_miles = $car->getMiles();
+                    $car_image = $car->getImage();
+
+                    $output = $output . "
+
+                    <h3>" . $car_make . "<h3>
+                   <img src=" . $car_image .">
+                    <ul>
+                         <li> $" . $car_price . "</li>
+                         <li> Miles: " . $car_miles . "</li>
+                     </ul>";
+                  }
+              }
+              return $output;
+
     });
 
     return $app;

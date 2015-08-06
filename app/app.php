10cbdfbe1cb6508abe9619ede2702f756e885fdf
Tyler Deem & alexandra brown <student@epicodus.com>
2015-08-06 11:27:44 -0700

    Attempt changes to add twig. So far, unsuccessful.

--- app/app.php
+++ app/app.php
@@ -4,32 +4,9 @@
 
     $app = new Silex\Application();
 
-    $app->get("/", function() {
-        return "
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
-            <title>Cars!!</title>
-        </head>
-        <body>
-          <h1>Find YOUR new car...</h1>
-          <div class='container'>
-            <form action='/car'>
-              <div class='form-group'>
-                <label for='price'>Enter your max price: $</label>
-                <input id='price' name='price' class='form-control' type='number'>
-              </div>
-              <div class='form-group'>
-                <label for='miles'>Enter your desired mileage</label>
-                <input id='miles' name='miles' class='form-control' type='number'>
-              </div>
-              <button type='submit' class='btn-danger'>Search now!</button>
-            </form>
-          </div>
-        </body>
-        </html>
-            ";
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('cars.html.twig', array('cars' => Car::getAll()));
+
     });
 
     $app->get("/car", function() {
@@ -37,13 +14,11 @@
         $second_car = new Car("Honda CRV", 7500, 140000, "img/crv.jpg");
         $third_car = new Car("Suburu Legacy", 7900, 130000, "img/legacy.jpg");
         $fourth_car = new Car("Tesla Model S", 200000, 5000, "img/tesla.jpg");
-
         $cars = array($first_car, $second_car, $third_car, $fourth_car);
 
         $cars_matching_search = array();
+
         foreach ($cars as $car) {
-          $car_price = $car->getPrice();
-          $car_miles = $car->getMiles();
           if (empty($_GET["price"])) {
             if ($car_miles < $_GET["miles"]) {
               array_push($cars_matching_search, $car);
--- src/Car.php
+++ src/Car.php
@@ -56,5 +56,6 @@
         {
             return $this->image;
         }
+        
     }
 ?>
